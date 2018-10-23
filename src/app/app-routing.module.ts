41719d10629dadc2e8be93b7e411193bd1b5ec7d
Jake Entrikin <jakeentrikin@Jakes-MacBook-Pro.local>
2018-10-23 00:06:15 -0600

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { LookbookComponent } from './lookbook/lookbook.component';
+import { ShopItemComponent } from './shop-item/shop-item.component';
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'shop', component: ShopComponent },
+  {path: 'shop-item', component: ShopItemComponent },
+  {path: 'lookbook', component: LookbookComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {useHash: false})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { LayoutComponent } from './layout/layout.component';
+import { ShopItemComponent } from './shop-item/shop-item.component';
+import { LookbookComponent } from './lookbook/lookbook.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    LayoutComponent,
+    ShopItemComponent,
+    LookbookComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
