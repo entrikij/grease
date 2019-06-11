792f4cfc34bd1f6bcebd7405880bf3c327ab1bd6
mattvitello <vitellofellow@gmail.com>
2019-06-10 22:42:19 -0400

    fixed imgs

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -213,7 +213,7 @@ export class ShopComponent implements OnInit {
     if(activeItemId === 'a'){
       this.price = 25;
       this.ClearOldBtns();
-      this.InitializeShopifyItem("1781270347833");
+      this.InitializeShopifyItem("soldout");
     }
     if(activeItemId === 'b'){
       this.price = 25;
